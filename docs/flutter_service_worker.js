2ce5630888903620fad0674c50603bef2dfc62a2
SAULTMILAN <singhmilan0976@gmail.com>
2025-11-11 10:09:44 -0500

    Fix base href for GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "59b0b5ab673c529ac874cb43c16f5667
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "935edb6860a0a47a74e858686f9ac7fb",
+"flutter_bootstrap.js": "387e4c83bc8c417567d4961bd7720f33",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
